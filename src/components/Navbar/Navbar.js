b4959186deed68bb78eee4721e6d62604d7ef085
kinshuk146 <100469913+kinshuk146@users.noreply.github.com>
2023-02-14 03:35:57 +0530

    Third Commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,10 +2,9 @@ import React from 'react'
 import './Navbar.css'
 function Navbar() {
   return (
-    // 
-      <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
+    <nav class="navbar navbar-expand-lg  sticky-top">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Hotel Grand 81</a>
+    <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -15,7 +14,7 @@ function Navbar() {
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">Rooms</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -32,10 +31,11 @@ function Navbar() {
           <a class="nav-link disabled">Disabled</a>
         </li>
       </ul>
+      <button type="button" class="btn btn-light">Book Now</button>
     </div>
   </div>
 </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
