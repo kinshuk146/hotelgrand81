5849e8ffc0cda8c5733d2d301fc7cc3b4a28ed13
kinshuk146 <100469913+kinshuk146@users.noreply.github.com>
2023-02-17 15:51:23 +0530

    finalised navbar

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,37 +1,93 @@
-import * as React from 'react';
-
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import { LocalizationProvider } from '@mui/x-date-pickers-pro';
-import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
-import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
-import Stack from '@mui/material/Stack';
-import Autocomplete from '@mui/material/Autocomplete';
+import React from 'react';
+import $ from 'jquery'; 
+import moment from 'moment';
 
 
+function Form() {
+  $(function () {
+    var bindDatePicker = function() {
+     $(".date").datetimepicker({
+         format:'YYYY-MM-DD',
+       icons: {
+         time: "fa fa-clock-o",
+         date: "fa fa-calendar",
+         up: "fa fa-arrow-up",
+         down: "fa fa-arrow-down"
+       }
+     }).find('input:first').on("blur",function () {
+       // check if the date is correct. We can accept dd-mm-yyyy and yyyy-mm-dd.
+       // update the format if it's yyyy-mm-dd
+       var date = parseDate($(this).val());
+ 
+       if (! isValidDate(date)) {
+         //create date based on momentjs (we have that)
+         date = moment().format('YYYY-MM-DD');
+       }
+ 
+       $(this).val(date);
+     });
+   }
+    
+    var isValidDate = function(value, format) {
+     format = format || false;
+     // lets parse the date to the best of our knowledge
+     if (format) {
+       value = parseDate(value);
+     }
+ 
+     var timestamp = Date.parse(value);
+ 
+     return isNaN(timestamp) === false;
+    }
+    
+    var parseDate = function(value) {
+     var m = value.match(/^(\d{1,2})(\/|-)?(\d{1,2})(\/|-)?(\d{4})$/);
+     if (m)
+       value = m[5] + '-' + ("00" + m[3]).slice(-2) + '-' + ("00" + m[1]).slice(-2);
+ 
+     return value;
+    }
+    
+    bindDatePicker();
+  });
 
-
-export default function BasicDateRangePicker() {
-  const [value, setValue] = React.useState([null, null]);
 
   return (
-    <div className="form" style={{padding:'1rem'}}>
-    <LocalizationProvider dateAdapter={AdapterDayjs} localeText={{ start: 'Check-in', end: 'Check-out' }}>
-      <DateRangePicker value={value} onChange={(newValue) => {
-          setValue(newValue);
-        }}
-        renderInput={(startProps, endProps) => (
-          <React.Fragment>
-            <TextField {...startProps} />
-            <Box sx={{ mx: 2 }}> to </Box>
-            <TextField {...endProps} />
-          </React.Fragment>
-        )}
-      />
-    </LocalizationProvider>
-
-<Autocomplete id="free-solo-demo" freeSolo  renderInput={(params) => <TextField {...params} label="freeSolo" />}></Autocomplete>
+    <div>
+      <div class="container">
+  <div class="row">
   </div>
-
-  );
+  <br />
+    <div class="row">
+        <div class='col-sm-3'>
+            <div class="form-group">
+                <div class='input-group date' id='datetimepicker1'>
+                    <input type='text' class="form-control" />
+                    <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+  <div class="row">
+  </div>
+  <br />
+    <div class="row">
+        <div class='col-sm-3'>
+            <div class="form-group">
+                <div class='input-group date' id='datetimepicker1'>
+                    <input type='text' class="form-control" />
+                    <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
+  )
 }
+
+export default Form
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,55 @@
 import React from 'react'
 import './Navbar.css'
+import  { useState, useEffect } from "react"
 
 
 function Navbar() {
+  const [navbar, setNavbar] = useState(false)
+  const [showNavbar,setshowNavbar]=useState(false)
+
+  //navbar scroll changeBackground function
+  const changeBackground = () => {
+    console.log(window.scrollY)
+    if (window.scrollY >= 20) {
+      setNavbar(true)
+    } else {
+      setNavbar(false)
+    }
+  }
+
+  const hideNavbar = () => {
+    console.log(window.scrollY)
+    if (window.scrollY < 20) {
+      setshowNavbar(true)
+    } else {
+      setshowNavbar(false)
+    }
+  }
+
+  useEffect(() => {
+    changeBackground()
+    // adding the event when scroll change background
+    window.addEventListener("scroll", changeBackground)
+  })
+
+  useEffect(() => {
+    hideNavbar()
+    // adding the event when scroll change background
+    window.addEventListener("scroll", hideNavbar)
+  })
+
   return (
-    <nav class="navbar navbar-expand-lg fixed-top">
+    <nav className={`navbar ${navbar ? "bg-black" : ""} navbar-expand-lg fixed-top`}>
   <div class="container-fluid">
   
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
     <a class="navbar-brand" href="#" style={{color:'white'}}><h3>Hotel Grand 81</h3></a>
-    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+    <div class="offcanvas offcanvas-start show text-bg-dark" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div class="offcanvas-header">
         <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <button type="button" class="btn-close btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -41,7 +76,7 @@ function Navbar() {
       </ul>
       </div>
     </div>
-    <button type="button" class="btn btn-light rounded-0"><div className="Book">BOOK NOW</div></button>
+    <button type="button" class="btn border border-white rounded-0"><div style={{color:'white'}}>BOOK NOW</div></button>
   </div>
 </nav>
   )
