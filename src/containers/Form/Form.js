dd089c3ddf2daf44ae63d6ca1fe23e3dc34dc112
kinshuk146 <100469913+kinshuk146@users.noreply.github.com>
2023-02-25 00:03:52 +0530

    LotOfChanges

--- src/containers/Form/Form.js
+++ src/containers/Form/Form.js
@@ -1,6 +1,23 @@
 import React from 'react'
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import DatePicker from '../../components/DatePicker';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import { MobileDateRangePicker } from '@mui/x-date-pickers-pro/MobileDateRangePicker';
+import { DesktopDateRangePicker } from '@mui/x-date-pickers-pro/DesktopDateRangePicker';
+import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
+import  { useState, useEffect } from "react"
+import { styled } from '@mui/system';
+
+const CustomButton = styled(Button)`
+background-color: black;
+color: white;
+
+&:hover {
+  background-color: white;
+  color: black;
+}`
 
 function Form() {
   return (
@@ -12,18 +29,24 @@ function Form() {
                 flexWrap: 'wrap',
                 justifyContent:'center',
                 '& > :not(style)': {
-                  m: 3,
+                  m: 2,
                   width: 1500,
-                  height:100
+                  height:90
                 },
+                
               }}
             >
-              <Paper/>
-              
+              <Paper square={true} children={
+                <div class="d-flex flex-row mb-3">
+                <div class="p-2 flex-grow-1 mt-2"><DatePicker square={true} /></div>
+                <div class="p-2 mt-2"><CustomButton size="small" variant="contained" sx={{borderRadius:'0'}}>Check Availability</CustomButton></div>
+                </div>
+              }/>
             </Box>
             </div>
     </div>
   )
 }
+              
 
 export default Form
