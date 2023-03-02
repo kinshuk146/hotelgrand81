a1972f77284f084c6e15d6f56c789feedb7d58f1
kinshuk146 <100469913+kinshuk146@users.noreply.github.com>
2023-03-03 04:51:47 +0530

    Added Room Gallery

--- src/containers/Rooms/Rooms.js
+++ src/containers/Rooms/Rooms.js
@@ -2,7 +2,7 @@ import React from 'react'
 import EachRoom from '../../components/EachRoom';
 function Rooms() {
   return (
-    <div style={{marginTop:'-2.5rem'}}>
+    <div style={{marginTop:'2rem'}}>
         <EachRoom roomName="Grand Suite" imagelink="./assets/SuperDeluxe1.jpg" price="₹3000"/>
         <EachRoom roomName="Super Deluxe Room" imagelink="./assets/SuperDeluxe1.jpg" price="₹3000"/>
         <EachRoom roomName="Deluxe Room" imagelink="./assets/SuperDeluxe1.jpg" price="₹3000"/>
